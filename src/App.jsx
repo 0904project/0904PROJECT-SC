8347161095f140cb386a2bec586a0701e3157fc4
farhan maulana <farhanmaulana1710@gmail.com>
2024-03-06 22:31:52 +0700

    frist project repost

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+function App() {
+
+  return (
+    <>
+      <h1 className={'text-2xl font-base p-5 bg-slate-800 text-center text-slate-300'}>
+        Hello in we project
+      </h1>
+    </>
+  )
+}
+
+export default App
